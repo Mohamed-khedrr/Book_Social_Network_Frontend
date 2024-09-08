482a8c3f68fefbd81084033a4be329c99c9a0f3c
Khedr <khedr66664@gmail.com>
2024-09-08 10:56:52 +0300

    login function , token service

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,8 +1,10 @@
+import { TokenService } from './../../services/token.service';
 import { Component, inject } from '@angular/core';
 import { LoginRequest } from '../../../api-services/models/login-request';
 import { FormsModule } from '@angular/forms';
 import { AuthenticationService } from '../../../api-services/services';
 import { Login$Params } from '../../../api-services/fn/authentication/login';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,20 +17,33 @@ export class LoginComponent {
   authRequest: LoginRequest = { email: '', password: '' };
   errMsg: Array<string> = [];
   authService = inject(AuthenticationService);
+  router = inject(Router);
+  tokenService = inject(TokenService);
 
   login() {
     const loginParams: Login$Params = {
       body: this.authRequest,
     };
     this.authService.login(loginParams).subscribe({
-      next(res) {
+      next: (res) => {
+        this.errMsg = [];
+        this.tokenService.setToken(res.token as string);
         console.log(res);
+        this.router.navigate(['/books']);
       },
-      error(err) {
+      error: (err) => {
+        this.errMsg = [];
         console.log(err);
+        if (err.error.validationErrors) {
+          this.errMsg.push(err.error.validationErrors[0]);
+        } else {
+          this.errMsg.push(err.error.errorDescription);
+        }
       },
     });
   }
 
-  register() {}
+  register() {
+    this.router.navigate(['/register']);
+  }
 }
