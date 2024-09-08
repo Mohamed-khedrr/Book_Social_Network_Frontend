1fcc7beffcb08e68e860a8d7f7372969a25e8794
Khedr <khedr66664@gmail.com>
2024-09-08 23:14:47 +0300

    login & register components & token service

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,7 +1,12 @@
 import { TokenService } from './../../services/token.service';
 import { Component, inject } from '@angular/core';
 import { LoginRequest } from '../../../api-services/models/login-request';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AuthenticationService } from '../../../api-services/services';
 import { Login$Params } from '../../../api-services/fn/authentication/login';
 import { Router } from '@angular/router';
@@ -9,41 +14,44 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  authRequest: LoginRequest = { email: '', password: '' };
-  errMsg: Array<string> = [];
   authService = inject(AuthenticationService);
   router = inject(Router);
   tokenService = inject(TokenService);
+  fb = inject(FormBuilder);
+  errMsg: string = '';
+
+  loginForm = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
 
   login() {
-    const loginParams: Login$Params = {
-      body: this.authRequest,
+    const loginBody: LoginRequest = {
+      email: this.loginForm.value.email as string,
+      password: this.loginForm.value.password as string,
     };
+
+    const loginParams: Login$Params = {
+      body: loginBody,
+    };
+
     this.authService.login(loginParams).subscribe({
       next: (res) => {
-        this.errMsg = [];
         this.tokenService.setToken(res.token as string);
-        console.log(res);
         this.router.navigate(['/books']);
       },
       error: (err) => {
-        this.errMsg = [];
-        console.log(err);
-        if (err.error.validationErrors) {
-          this.errMsg.push(err.error.validationErrors[0]);
-        } else {
-          this.errMsg.push(err.error.errorDescription);
-        }
+        this.errMsg = err.error?.errorDescription;
       },
     });
   }
 
-  register() {
+  toRegisterPage() {
     this.router.navigate(['/register']);
   }
 }
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { TokenService } from '../../services/token.service';
+import { AuthenticationService } from '../../../api-services/services';
+import { Register$Params } from '../../../api-services/fn/authentication/register';
+import { RegistrationRequest } from '../../../api-services/models/registration-request';
 
 @Component({
   selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  authService = inject(AuthenticationService);
+  router = inject(Router);
+  tokenService = inject(TokenService);
+  fb = inject(FormBuilder);
+  errMsg: string = '';
 
-  constructor() { }
+  registerForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  registerNewAccount() {
+    const loginBody: RegistrationRequest = {
+      email: this.registerForm.controls.email.value as string,
+      password: this.registerForm.controls.password.value as string,
+      firstName: this.registerForm.controls.firstName.value as string,
+      lastName: this.registerForm.controls.lastName.value as string,
+    };
+
+    const registerParams: Register$Params = {
+      body: loginBody,
+    };
+
+    this.errMsg = '';
+
+    this.authService.register(registerParams).subscribe({
+      next: (res) => {
+        this.router.navigate(['/activate-account']);
+      },
+      error: (err) => {
+        console.log(err);
+        this.errMsg = err.error?.errorDescription || err.error.error;
+      },
+    });
   }
 
+  toLoginPage() {
+    console.log(this.registerForm.touched);
+    // this.router.navigate(['/login']);
+  }
 }
